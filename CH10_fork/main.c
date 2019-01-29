33d62c6ea2ef4a1e177636dd0fb260c110192beb
Jan <j.b.liefting@gmail.com>
2019-01-29 18:39:28 +0100

    first go

---
+++ CH10_fork/main.c
@@ -0,0 +1,40 @@
+/*
+ * main.c
+ *
+ *  Created on: Aug 19, 2018
+ *      Author: jan
+ */
+
+//includes
+
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+
+int main(int argc, char *argv[]) {
+
+	char *feeds[] = { "http://feeds.bbci.co.uk/news/rss.xml" };
+	int times = 1;
+	char *phrase = argv[1];
+	int i;
+	for (i = 0; i < times; i++) {
+		char var[255];
+		sprintf(var, "RSS_FEED=%s", feeds[i]);
+		char *vars[] = { var, NULL };
+
+//		pid_t pid = fork();
+//		if (pid == -1) { //there is an issue with the forking
+//			fprintf(stderr, "Can't fork the process %s\n", strerror(errno));
+//			return 1;
+//		}
+//		if (!pid) { // this is executed by the child process
+			fprintf(stdout,"Looking for: %s\n", phrase);
+			if (execle("/usr/bin/python", "/usr/bin/python", "/home/jan/Desktop/rssgossip.py",
+					phrase, NULL, vars) == -1) {
+				fprintf(stderr, "Can't start Python %s\n", strerror(errno));
+				return 1;
+//			}
+		}
+	}
+}

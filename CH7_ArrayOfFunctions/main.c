419dcb9b6ecdb79893b2e484bfce6981af81c4d2
Jan <j.b.liefting@gmail.com>
2019-01-29 18:53:26 +0100

    start

---
+++ CH7_ArrayOfFunctions/main.c
@@ -0,0 +1,48 @@
+/*
+ * main.c
+ *
+ *  Created on: Aug 9, 2018
+ *      Author: jan
+ */
+
+//includes
+#include<stdio.h>
+
+//globals
+enum response_type {
+	DUMP, SECOND_CHANCE, MARRIAGE
+};
+typedef struct {
+	char *name;
+	enum response_type type;
+} response;
+
+//helpers
+void dump(response r) {
+	printf("Dear %s,\n", r.name);
+	puts("Unfortunately your last date contacted us to");
+	puts("say that they will not be seeing you again");
+}
+void second_chance(response r) {
+	printf("Dear %s,\n", r.name);
+	puts("Good news: your last date has asked us to");
+	puts("arrange another meeting. Please call ASAP.");
+}
+void marriage(response r) {
+	printf("Dear %s,\n", r.name);
+	puts("Congratulations! Your last date has contacted");
+	puts("us with a proposal of marriage.");
+}
+
+//array with functions
+void (*replies[])(response) = {dump, second_chance, marriage};
+
+int main() {
+	response r[] = { { "Mike", DUMP }, { "Luis", SECOND_CHANCE }, { "Matt",
+			SECOND_CHANCE }, { "William", MARRIAGE } };
+	int i;
+	for (i = 0; i < 4; i++)
+		(*replies[r[i].type])(r[i]); // choose the right function using the ENUM type
+
+	return 0;
+}

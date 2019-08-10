50bd2044ed4664f669aa9e936a9391975e330a4f
jan <jan@jan-G41M-ES2L>
2019-08-10 14:55:08 +0200

    2

--- HTTP client/src/main.c
+++ HTTP client/src/main.c
@@ -50,6 +50,52 @@ int parse_url(char *uri, char **host, char **path)
 	return 0;
 }
 
+#define MAX_GET_COMMAND 255
+/**
+ * Format and send an HTTP get command. The return value will be 0
+ * on success, -1 on failure, with errno set appropriately. The caller
+ * must then retrieve the response.
+ */
+int http_get(int connection, const char *path, const char *host)
+{
+	static char get_command[MAX_GET_COMMAND];
+	sprintf(get_command, "GET /%s HTTP/1.1\r\n", path);
+	printf("%s",get_command);
+	if(send(connection, get_command, strlen(get_command), 0) == -1)
+	{
+		return -1;
+	}
+	sprintf(get_command, "Host: %s\r\n", host);
+	printf("%s",get_command);
+	if(send(connection, get_command, strlen(get_command), 0) == -1)
+	{
+		return -1;
+	}
+	sprintf(get_command, "Connection: close\r\n\r\n");
+	printf("%s",get_command);
+	if(send(connection, get_command, strlen(get_command), 0) == -1)
+	{
+		return -1;
+	}
+	return 0;
+}
+
+#define BUFFER_SIZE 255
+/**
+ * Receive all data available on a connection and dump it to stdout
+ */
+void display_result(int connection)
+{
+	int received = 0;
+	static char recv_buf[BUFFER_SIZE + 1];
+	while((received = recv(connection, recv_buf, BUFFER_SIZE, 0)) > 0)
+	{
+		recv_buf[received] = '\0';
+		printf("%s", recv_buf);
+	}
+	printf("\n");
+}
+
 /**
  * Simple command-line HTTP client.Chapter 1
  */
@@ -62,16 +108,51 @@ int main(int argc, char *argv[])
 
 	if(argc < 2)
 	{
-		fprintf( stderr, "Usage: %s: <URL>\n", argv[ 0 ] );
+		fprintf(stderr, "Usage: %s: <URL>\n", argv[0]);
 		return 1;
 	}
 	if(parse_url(argv[1], &host, &path) == -1)
 	{
-		fprintf( stderr, "Error - malformed URL ‘%s’.\n", argv[ 1 ] );
+		fprintf(stderr, "Error - malformed URL ‘%s’.\n", argv[1]);
 		return 1;
 	}
-	printf( "Connecting to host ‘%s’\n", host );
-	printf("Looking for file %s\n", path);
+	printf("Connecting to host ‘%s’\n", host);
+	printf("Looking for file ‘%s‘\n", path);
+
+	// Step 1: open a socket connection on http port with the destination host.
+	client_connection = socket(PF_INET, SOCK_STREAM, 0);
+	if(!client_connection)
+	{
+		perror("Unable to create local socket");
+		return 2;
+	}
+	host_name = gethostbyname(host);
+	if(!host_name)
+	{
+		perror("Error in name resolution");
+		return 3;
+	}
+
+	host_address.sin_family = AF_INET;
+	host_address.sin_port = htons(HTTP_PORT);
+	memcpy(&host_address.sin_addr, host_name->h_addr_list[0],
+			sizeof(struct in_addr));
+	if(connect(client_connection, (struct sockaddr *)&host_address,
+			sizeof(host_address)) == -1)
+	{
+		perror("Unable to connect to host");
+		return 4;
+	}
+	printf("Retrieving document: ‘%s’\n", path);
+
+	http_get(client_connection, path, host);
+	display_result(client_connection);
+	printf("Shutting down.\n");
+	if(close(client_connection) == -1)
+	{
+		perror("Error closing client connection");
+		return 5;
+	}
 
 	return 0;
 }
